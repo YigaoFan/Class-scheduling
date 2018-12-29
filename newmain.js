04a3e3e1b27ad403f0a84656e5dead6cdd0dba92
Yigao Fan <v-yifan@microsoft.com>
2018-12-29 20:11:01 +0800

    add some comment.

--- newmain.js
+++ newmain.js
@@ -1,52 +1,53 @@
 var mouse = {
-	currentLocation: {
-		x: null,
-		y: null,
-	},
-	recentClickedLocation: {
-		x: null,
-		y: null,
-	},
-	validClickLocation: [],
+  currentLocation: {
+    x: null,
+    y: null,
+  },
+  recentClickedLocation: {
+    x: null,
+    y: null,
+  },
+  validClickLocation: [],
 }
 
 
-
+// mouse event
 window.addEventListener('mousemove', function(event) {
-	mouse.x = event.clientX - 6
-	mouse.y = event.clientY - 6
+  mouse.x = event.clientX - 6
+  mouse.y = event.clientY - 6
 })
 
 window.addEventListener('mousedown', function(event) {
-	mouse.recentClickedLocation.x = event.clientX - 6
-	mouse.recentClickedLocation.y = event.clientY - 6
+  mouse.recentClickedLocation.x = event.clientX - 6
+  mouse.recentClickedLocation.y = event.clientY - 6
 })
 
 var sreen = Screen(document.querySelector('#id-canvas').getContext('2d'), 8, 8)
 var weekManagement = WeekManagement(Screen())
 
+// operation button
 document.querySelector('#id-add-day', function('click', function(event) {
-	weekManagement.addWeek()
+  weekManagement.addWeek()
 }))
 
 document.querySelector('#id-del-day', function('click', function(event) {
-	weekManagement.removeWeek()
+  weekManagement.removeWeek()
 }))
 
 var __main = function() {
-	var canvas = document.querySelector('#id-canvas')
-	var context = canvas.getContext('2d')
+  var canvas = document.querySelector('#id-canvas')
+  var context = canvas.getContext('2d')
 
-	weekManagement.check = function() {
-		re
-	}
+  weekManagement.check = function() {
+    re
+  }
 
-	while(true) {
-		switch(weekManagement.check()) {
-			case
-		}
-		weekManagement.
-	}
+  while (true) {
+    switch (weekManagement.check()) {
+      case
+    }
+    weekManagement.
+  }
 }
 
 __main()
